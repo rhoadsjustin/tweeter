6f168cab5d234cdb069f6e331203a7bc3ea19ada
Justin Rhoads <rhoadsjustin@gmail.com>
2017-12-13 17:37:31 -0600

    utlizied structure of twitter clone from previous app

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import { Provider, connect } from 'react-redux';
+import { Font, AppLoading } from 'expo'
+import Router from './config/routes';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('native-base/Fonts/Ionicons.ttf')
+    });
+
+    this.setState({ isReady: true });
+  }
+
+
   render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider>
+        <Router />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('main', () => App);
